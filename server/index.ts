0eab978a7a32bf02b0b3f02dc2de14bc4bc97894
Michael Becker <michael.becker@instacart.com>
2025-04-22 17:04:47 -0400

    fix: update server to bind to 0.0.0.0 and properly parse PORT

--- server/index.ts
+++ server/index.ts
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === "production") {
   await setupDevelopment(app, server);
 }
 
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+server.listen(port, "0.0.0.0", () => {
+  console.log(`Server running at http://0.0.0.0:${port}`);
 });
